2c0d54e08bc913a9b0556f8997f6b3cabfbcfae4
Peder Vatn Austad <peder.vatn.austad@gmail.com>
2022-09-27 14:20:10 +0200

    ahhhhhah

--- databaser/node_database_api/frontend_egg/js/get_request.js
+++ databaser/node_database_api/frontend_egg/js/get_request.js
@@ -23,10 +23,35 @@ function updRequest(){
     var nem2 = document.getElementById("test.4").value;
     var nem3 = document.getElementById("test.5").value;
     var nem4 = document.getElementById("test.6").value;
-    fetch('http://localhost:3000/upd/' + "%60test2%60='" +  nem2 + "',%60test3%60='" + nem3 + "',%60test4%60='" + nem4 + "'WHERE%60test%60=" + nem)
+    fetch('http://localhost:3000/upd/' + "test2='" +  nem2 + "',test3='" + nem3 + "',test4='" + nem4 + "'WHERE test=" + nem)
         .then((response) => response.json())
         .then((data) => console.log(data))
-        .then(document.getElementById('logger').innerHTML = "sent the thing");
+        .then(document.getElementById('logger3').innerHTML = "sent the thing");
 }
 //`test2`='[value-2]',`test3`='[value-3]',`test4`='[value-4]'WHERE`test`=0
 //%60test2%60='[vale-2]',%60test3%60='[value-3]',%60test4%60='[value-4]'WHERE%60test%60=4
+
+
+function tabl() {
+    console.log("tbl");
+    fetch('http://localhost:3000/get')
+        .then((response) => response.json())
+        .then(
+            
+            data => {
+                console.log(data);
+                if (data.length > 0) {
+        
+                  var temp = "<tr><th> test </th><th> test2 </th><th> test3 </th><th> test4 </th></tr>";
+                  data.forEach((itemData) => {
+                    temp += "<tr>";
+                    temp += "<td>" + itemData.test + "</td>";
+                    temp += "<td>" + itemData.test2 + "</td>";
+                    temp += "<td>" + itemData.test3 + "</td>";
+                    temp += "<td>" + itemData.test4 + "</td>";
+                  });
+                  document.getElementById('Table').innerHTML = temp;
+                }
+            }
+        );
+}
